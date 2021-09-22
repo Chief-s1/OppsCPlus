587cb53e3f0e9736be3967a98b8dc96a8e53d801
Chief-s1 <90816577+Chief-s1@users.noreply.github.com>
2021-09-22 09:53:53 +0530

    Add files via upload

---
+++ Class_obj/day31.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+//#include<stdio.h>
+//#include<string.h>
+using namespace std;
+
+int main(void)
+{
+cout<<"Hello World!!";
+/*
+char ch[50],i,l,c;
+fgets(ch,50,stdin);
+l=strlen(ch);
+for(i=0;i<l-1;i++)
+{
+if(ch[i]=='1')
+c++;
+}
+
+for(i=0;i<c;i++)
+printf("1");
+for(i=0; i<(l-c); i++)
+printf("0") ;
+*/
+}
