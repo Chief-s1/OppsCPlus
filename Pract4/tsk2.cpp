9e34d4892220c103941da668697a7a87c45c79ea
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2021-09-29 10:34:06 +0530

    Create tsk2.cpp

---
+++ Pract4/tsk2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+namespace one
+{
+int add(int a, int b)
+{
+return (a+b);
+}
+
+}
+namespace two
+{
+float add (float a,float b)
+{
+return (a+b);
+}
+
+}
+using namespace one;
+using namespace two;
+
+int main(void)
+{
+cout<<add(1,2)<<endl;
+cout<<add(6.1,2.2)<<endl;
+}
