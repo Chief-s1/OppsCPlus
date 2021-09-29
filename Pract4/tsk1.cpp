693e7bbba35fad645ead7897ca0c4d5589aa493d
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2021-09-29 10:28:03 +0530

    Update tsk1.cpp

--- Pract4/tsk1.cpp
+++ Pract4/tsk1.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
+namespace one
+{
+int add(int a, int b)
+{
+return (a+b);
+}
+
+}
+namespace two
+{
+float add (float a,float b)
+{
+return (a+b);
+}
+
+}
+
 int main(void)
 {
-  cout<<"Hello World";
+cout<<one::add(1,2)<<endl;
+cout<<two::add(6.1,2.2)<<endl;
 }
