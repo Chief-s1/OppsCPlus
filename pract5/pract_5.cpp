4f4b3b90ec9b04da34ca11a244fceeeee4d36fd9
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2021-09-30 10:21:12 +0530

    Add files via upload

---
+++ pract5/pract_5.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include<limits.h>
+#include<float.h>
+using namespace std;
+
+void intfuc()
+{
+cout<<"Int Size:"<<sizeof(int)<<"B\n";
+cout<<"Range:("<<INT_MIN<<" to "<<INT_MAX<<")\n";
+}
+
+void charfuc()
+{
+cout<<"Char Size:"<<sizeof(char)<<"B\n":
+cout<<"Range:(0-255)\n";
+}
+
+void boolfuc()
+{
+cout<<"Bool Size:"<<sizeof(bool)<<"B\n";
+cout<<"Range:((0/1),(T/F))\n";
+}
+
+void shortfuc()
+{
+cout<<"ShortInt Size:"<<sizeof(short)<<"B\";
+cout<<"Range:("<<SCHAR_MIN<<" to "<<SCHAR_MAX<<")\n";
+}
+
+void longfuc()
+{
+cout<<"Long Size:"<<sizeof(long)<<"B\n";
+cout<<"Range:("<<LONG_MIN<<" to "<<LONG_MAX<<"
+}
+
+void floatfuc()
+{
+cout<<"Float:"<<sizeof(float)<<"B\n";
+cout<"Range:("<<FLT_MIN<<" to "<<FLT_MAX<<")\n";
+cout<<"Float has 7 decimal digits of precision\n";
+}
+
+void doublefuc()
+{
+cout<<"Double Size:"<<sizeof(double)<<"B\n";
+cout<<"Range:("<<DBL_MIN<<" to "<<DBL_
+cout<<"Double has 15 decimal digits of precision";
+}
+
+void lodoubfuc()
+{
+cout<<"Long Double Size:"<<sizseof(long double)<<"B\n";
+cout<<"Range:("<<LDBL_MIN<<" to "<<LDBL_MAX<<")\n";
+}
+ 
+void wcharfuc()
+{
+cout<<"Wide Char Size:"<<sizeof(wchar_t)<<"B\n"
+cout<<"Range:("<<WCHAR_MIN<<" to "<<WCHAR_MAX<<")\n";
+}
+
+int main(void)
+ {
+ 
+ 
+ 
+ }
+ 
+ 
+ 
