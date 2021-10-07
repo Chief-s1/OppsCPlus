054be8284dfb2040d463a18234d1a77f38a709f8
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2021-10-07 10:05:42 +0530

    Add files via upload

---
+++ tsk6/tsk1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+
+int a = 10, b = 5 ;
+string s="Hello",t="World";
+
+cout << a << " + " << b << " = " << a + b << endl ;      
+  
+cout<<s+t<<endl;
+    
+    cout << a + "string" << endl ;
+
+    return 0 ;
+}
