8559175d3a30343547c49c2f4167ccd8db38970c
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2022-01-07 12:57:36 +0530

    Create tsk4.cpp

---
+++ pract 7/tsk4.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using namespace std;
+int main ()
+{
+int k=1 ;
+int arr[3][3] ;
+
+
+for(int i = 0 ; i < 3 ; i++)
+{
+for(int j = 0 ; j < 3 ; j++)
+{
+    arr[i][j] = k ;
+    k++;
+}
+}
+
+
+for(int i = 0 ; i < 3 ; i++)
+{
+for(int j = 0 ; j < 3 ; j++)
+{
+
+
+    arr[i][j] = 10 - arr[i][j];
+}
+}
+
+
+for(int  i = 0 ; i < 3 ; i++)
+{
+for(int j = 0 ; j < 3 ; j++)
+{
+
+
+cout << arr[i][j] << "  ";
+}
+cout<<endl;
+}
+} 
+
+
+
+
+
+
