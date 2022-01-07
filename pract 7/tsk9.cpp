eadc0bd3048fd5e846c9cafd9a7df7d336b9a973
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2022-01-07 12:59:50 +0530

    Create tsk9.cpp

---
+++ pract 7/tsk9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+	using namespace std;
+	int main(){
+		int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+		for(int i=1;i<=3;i++){
+			for(int j=1;j<=3;j++){
+				arr[i-1][j-1]=arr[i-1][2]/(3);
+			}
+		}
+		for(int i=1;i<=3;i++){
+			for(int j=1;j<=3;j++){
+				cout<<arr[i-1][j-1];
+			}
+			cout<<endl;
+		}
+		return 0;
+	}
+
