e7a4e4313bfd097f8bd6cd6c212a8c7f6c5abacd
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2022-01-07 12:56:17 +0530

    Create tsk2.cpp

---
+++ pract 7/tsk2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std ;
+int main ()
+{
+int k = 1 ;
+int arr[3][3];
+for(int i = 0 ; i < 3 ; i++)
+{
+    for(int j =0  ; j < 3 ; j++)
+    {
+        arr[i][j] = k ;
+        k++;
+    }
+}
+
+
+for(int i = 2 ; i >= 0 ; i--)
+{
+for(int j = 2 ; j>=0 ; j--)
+{
+    cout << arr[i][j] << " ";
+    }
+    cout << endl ;
+    
+  }
+}
+
+
