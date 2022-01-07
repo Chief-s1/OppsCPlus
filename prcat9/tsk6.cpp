723fb514869a99824c4421db1d2d0c646501ace9
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2022-01-07 13:11:05 +0530

    Create tsk6.cpp

---
+++ prcat9/tsk6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+	 using namespace std;
+	class college {
+	    string name; 
+	    int roll_no;
+	};
+	struct student
+	{
+	  string name;
+	  int roll_no.
+	}
+	int main()
+	{
+	  college t;
+	  student s1;
+	  t.name="Swayam";// error because x is private
+	  t.x = 58; //error concept of data hiding
+	  s1.name="Shivam";
+	  s1.roll_no=57;
+	  getchar();
+	  return 0;
+	}
+
+ 
