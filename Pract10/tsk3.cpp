514abac6a488541b6d2919d48cca08cdf8c98f24
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2022-01-07 13:15:47 +0530

    Create tsk3.cpp

---
+++ Pract10/tsk3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+	using namespace std;
+	class rem{
+	int a,b;
+	public:
+	rem(int a,int b)
+	{
+	  this->a=a;
+	  this->b=b;
+	  cout<<this->a/this->b;
+	}
+	};
+	int main()
+	{
+	 rem obj(78,23);
+	 }
+
+ 
