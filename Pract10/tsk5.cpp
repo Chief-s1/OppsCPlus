68e8dbc27190ca791b18a759a03b5aebaf897fb2
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2022-01-07 13:16:27 +0530

    Create tsk5.cpp

---
+++ Pract10/tsk5.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+	using namespace std;
+	
+class abc{
+	private:
+	int num;
+
+	public:
+	abc(int x)
+	{
+	this->num=x;
+	}
+
+	void operator +(abc obj)
+	{
+	 cout<<num+obj.num;
+	}
+
+	};
+
+	int main()
+	{
+	  abc obj(10);
+	  abc obj1(20);
+	  obj+obj1;
+	  }
+
+ 
