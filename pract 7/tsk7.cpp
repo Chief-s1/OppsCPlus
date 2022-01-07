e0f44eccf32f7c3d75b629747b1ea1a13e8e9001
Swayam Manori <90816577+Chief-s1@users.noreply.github.com>
2022-01-07 12:58:34 +0530

    Create tsk7.cpp

---
+++ pract 7/tsk7.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+	using namespace std ;
+	int main ()
+	{
+	int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+	for(int i = 0 ; i < 3 ; i++)
+	{
+	for(int j = 0 ; j < 3 ; j++)
+	{
+	arr[i][j] -= 2*i+j;
+	}
+	}
+	for(int i = 1 ; i <= 3 ; i++)
+	{
+	for(int j = 1 ; j <= 3 ; j++)
+	{
+	cout<<arr[i-1][j-1];
+	}
+	cout << endl ;
+	}
+	return 0 ;
+	}
+
+
